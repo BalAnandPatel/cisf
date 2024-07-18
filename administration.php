265d9a8afc5ec3e46cb7f1da78ac48ac2d55b718
subhash patel <subhashgipl121@mail.com>
2024-07-18 19:10:27 +0530

    assoiceat list

--- administration.php
+++ administration.php
@@ -27,16 +27,83 @@ include "include/header.php";
     <tr>
       <th scope="col">#</th>
       <th scope="col">NAME</th>
-      <th scope="col">RANK</th>
-      <th scope="col">STATUS</th>
+      <th scope="col">Designation</th>
+      <th scope="col">Mobile NO.</th>
     </tr>
   </thead>
   <tbody>
     <tr>
+    <tr>
       <th scope="row">1</th>
-      <td>Shri Balbir Singh Mehta</td>
-      <td>Sr.COX</td>
-      <td>Rtd</td>
+      <td>Sh. Jai Bhagwan</td>
+      <td>President</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Sh. Suresh Chandra</td>
+      <td>Vice President(Central Co-ordination)</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Sh. Kadam Raj Singh</td>
+      <td>General Secretary</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Sh. Om Prakash Bhatt</td>
+      <td>Treasurer</td>
+      <td>NA</td>
+    </tr>  
+    <tr>
+      <th scope="row">5</th>
+      <td>Sh. Ashok Solahiya</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>  
+    <tr>
+      <th scope="row">6</th>
+      <td>Sh. Sanatan Senapati</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>  
+    <tr>
+      <th scope="row">7</th>
+      <td>Sh. Harjinder Singh</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Sh. Balbir Chand Mehta</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Sh. Hari Sadhan</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Sh. Indraj Singh</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Sh. Ram Yagya Mishra</td>
+      <td>Executive Member</td>
+      <td>NA</td>
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>Sh. Prem Kumar</td>
+      <td>Executive Member</td>
+      <td>NA</td>
     </tr>
     <tr>
   </tbody>
--- statecordintor.php
+++ statecordintor.php
@@ -36,10 +36,136 @@ include "include/header.php";
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Shri Balbir Singh Mehta</td>
-      <td>Sr.COX</td>
-      <td>Rtd</td>
-      <td>XXXXXXX</td>
+      <td>Shri Manik Mukharji</td>
+      <td>NA</td>
+      <td>W.B.</td>
+      <td>9674831686</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Shri Sunil Mishra</td>
+      <td>NA</td>
+      <td>ODISHA</td>
+      <td>8104513682</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Shri S.K. Mishra</td>
+      <td>NA</td>
+      <td>JHARKHAND</td>
+      <td>7208904893</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Shri Sailesh Kumar Mishra</td>
+      <td>NA</td>
+      <td>U.P. East</td>
+      <td>9519647331</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Shri Yashwant Singh</td>
+      <td>NA</td>
+      <td>U.P. West</td>
+      <td>7597317350</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Shri V.K. Thapa</td>
+      <td>NA</td>
+      <td>Himanchal</td>
+      <td>9882317621</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Shri S.C. Saklami</td>
+      <td>NA</td>
+      <td>UTTRA KHAND</td>
+      <td>9634759608</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Shri H.S.Uppal</td>
+      <td>NA</td>
+      <td>PUNJAB</td>
+      <td>8447594970</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Shri Balbir Mahta</td>
+      <td>NA</td>
+      <td>HARYANA</td>
+      <td>8708699712</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Shri Akhilesh Kr Divedi</td>
+      <td>NA</td>
+      <td>M.P.</td>
+      <td>7000798343</td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Shri S.V. Bhai</td>
+      <td>NA</td>
+      <td>GUJRAT</td>
+      <td>9574265325</td>
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>Shri B.K. Yamuna</td>
+      <td>NA</td>
+      <td>CHATTISGARH</td>
+      <td>9406361297</td>
+    </tr>
+    <tr>
+      <th scope="row">13</th>
+      <td>Shri K.N. Mathur</td>
+      <td>NA</td>
+      <td>RAJSTHAN</td>
+      <td>982847390</td>
+    </tr>
+    <tr>
+      <th scope="row">14</th>
+      <td>Shri A.K.Khnaujiya</td>
+      <td>NA</td>
+      <td>J.S.K.</td>
+      <td>7982602826</td>
+    </tr>
+    <tr>
+      <th scope="row">15</th>
+      <td>Shri C.K. Pandiya</td>
+      <td>NA</td>
+      <td>TAMILNADU</td>
+      <td>9552100761</td>
+    </tr>
+    <tr>
+      <th scope="row">16</th>
+      <td>Shri C.K. Tiwari</td>
+      <td>NA</td>
+      <td>BIHAR</td>
+      <td>7779827555</td>
+    </tr>
+    <tr>
+      <th scope="row">17</th>
+      <td>Shri Shyam Sundar Raddy</td>
+      <td>NA</td>
+      <td>TELENGANA</td>
+      <td>8985624380</td>
+    </tr>
+    <tr>
+      <th scope="row">18</th>
+      <td>Shri S.S. Mandlik</td>
+      <td>NA</td>
+      <td>MAHARASHRA</td>
+      <td>9819071333</td>
+    </tr>
+    <tr>
+      <th scope="row">19</th>
+      <td>Shri Nagen Baghaliya</td>
+      <td>NA</td>
+      <td>ASSUM</td>
+      <td>9678510225</td>
     </tr>
     </tbody>
 </table>
