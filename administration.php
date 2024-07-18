146a54c0b8406661a8e403705edb3dde08ab6280
Bal anand <giplanand@gmail.com>
2024-07-18 17:07:22 +0530

    fn

---
+++ administration.php
@@ -0,0 +1,51 @@
+
+<?php
+include "include/header.php";
+?>
+<!-- breadcrumb Wrapper Start -->
+<div class="breadcrumb-wrapper">
+  <div class="container">
+    <div class="banner-title">
+      <h2>Administration Body</h2>
+      <div class="line"> <span></span></div>
+    </div>
+    <ul class="inner-breadcrumb">
+      <li><a href="index.php">Home</a></li>
+      <li>Administration Body</li>
+    </ul>
+  </div>
+</div>
+
+<div class="inner-page-wrapper">
+  <div class="container">
+  <div class="title">
+      <h2><span>List of Administration Body</span></h2>
+      <!-- <h3>If the job is online, you will find it on RecruitePro</h3> -->
+    </div>  
+    <table class="table table-striped">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">NAME</th>
+      <th scope="col">RANK</th>
+      <th scope="col">STATUS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Shri Balbir Singh Mehta</td>
+      <td>Sr.COX</td>
+      <td>Rtd</td>
+    </tr>
+    <tr>
+  </tbody>
+</table>
+</div>
+</div>
+<!-- breadcrumb Wrapper End --> 
+<!-- Inner page Wrapper Start -->
+<!-- Inner page Wrapper End --> 
+<?php
+include "include/footer.php";
+?>
---
+++ statecordintor.php
@@ -0,0 +1,53 @@
+
+<?php
+include "include/header.php";
+?>
+<!-- breadcrumb Wrapper Start -->
+<div class="breadcrumb-wrapper">
+  <div class="container">
+    <div class="banner-title">
+      <h2>State Body</h2>
+      <div class="line"> <span></span></div>
+    </div>
+    <ul class="inner-breadcrumb">
+      <li><a href="index.php">Home</a></li>
+      <li>State Body</li>
+      <li></li>
+    </ul>
+  </div>
+</div>
+
+<div class="inner-page-wrapper">
+  <div class="container">
+  <div class="title">
+      <h2><span>State Body of Association</span></h2>
+      <!-- <h3>If the job is online, you will find it on RecruitePro</h3> -->
+    </div>  
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">NAME</th>
+      <th scope="col">RANK</th>
+      <th scope="col">STATE</th>
+      <th scope="col">MOBILE</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Shri Balbir Singh Mehta</td>
+      <td>Sr.COX</td>
+      <td>Rtd</td>
+      <td>XXXXXXX</td>
+    </tr>
+    </tbody>
+</table>
+</div>
+</div>
+<!-- breadcrumb Wrapper End --> 
+<!-- Inner page Wrapper Start -->
+<!-- Inner page Wrapper End --> 
+<?php
+include "include/footer.php";
+?>
